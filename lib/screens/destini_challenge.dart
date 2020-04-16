18d7f183435f6db3bb066d286bccb06898acd2f1
IzzatMohd <izart1994@gmail.com>
2020-04-16 17:55:37 +0800

    second_commit

--- lib/screens/destini_challenge.dart
+++ lib/screens/destini_challenge.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import '../screens-child/destini_challange_page.dart';
 import '../widgets/main_drawer.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 class DestiniChallange extends StatelessWidget {
   static const routeName = '/destinichallange';
   const DestiniChallange({Key key}) : super(key: key);
